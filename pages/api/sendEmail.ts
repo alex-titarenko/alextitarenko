f785a78e1f930c21ed4b654dcc72cd9f3b2a2282
Alex Titarenko <alx.titarenko@gmail.com>
2019-12-23 20:53:44 -0800

    update sendEmail api

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -1,5 +1,17 @@
 import { NowRequest, NowResponse } from '@now/node'
+import fetch from 'isomorphic-fetch'
+import sgMail from '@sendgrid/mail'
 
-export default (req: NowRequest, res: NowResponse) => {
+
+export default async function (req: NowRequest, res: NowResponse) {
+  const secret_key = process.env.SECRET_KEY;
+  const token = req.body.token;
+
   res.json({ name: 'John', email: 'john@example.com' })
+}
+
+async function verifyRecaptchaAsync(recaptchaSecretKey: string, recaptchaToken: string) {
+
+  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${recaptchaSecretKey}&response=${recaptchaToken}`;
+  const response = await fetch(url, { method: 'post' });
 }
