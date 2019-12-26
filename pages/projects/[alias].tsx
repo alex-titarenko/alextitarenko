92688d31f136ba42c1b12a4aad4ca0843c448692
Alex Titarenko <alx.titarenko@gmail.com>
2019-12-26 11:44:12 -0800

    code refactoring

--- pages/projects/[alias].tsx
+++ pages/projects/[alias].tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useEffect } from 'react'
 import Head from 'next/head'
 import Layout from '../../components/Layout'
 import { Project } from '../../models/Project'
