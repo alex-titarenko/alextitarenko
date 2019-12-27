e6c82924a3c5d1393682f573dff9c05460a7a9a5
Alex Titarenko <alx.titarenko@gmail.com>
2019-12-26 21:31:04 -0800

    fix blog post page

--- pages/blog/archive/[year]/[month]/[slug].tsx
+++ pages/blog/archive/[year]/[month]/[slug].tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Disqus from 'disqus-react'
 import Head from 'next/head'
 import Layout from '../../../../../components/Layout'
@@ -31,87 +31,85 @@ export async function unstable_getStaticProps({ params }) {
   return { props: post };
 }
 
-export default class BlogPostPage extends React.Component<BlogPost> {
-  public componentDidMount() {
-    if (this.props.published) {
-      Analytics.logEvent('BlogPost', 'View', this.props.title);
+export default function BlogPostPage(props: React.PropsWithChildren<BlogPost>) {
+  useEffect(() => {
+    if (props.published) {
+      Analytics.logEvent('BlogPost', 'View', props.title);
     }
-  }
+  });
 
-  public render() {
-    const imageUrl = /^http(s)?:/i.test(this.props.image) ?
-      this.props.image :
-      new URL(`/images/posts/${this.props.urlSlug}/${this.props.image}`, appConfig.canonicalBaseUrl).href;
+  const imageUrl = /^http(s)?:/i.test(props.image) ?
+    props.image :
+    new URL(`/images/posts/${props.urlSlug}/${props.image}`, appConfig.canonicalBaseUrl).href;
 
-    const disqusConfig = {
-      identifier: this.props.id ?? this.props.urlSlug,
-      title: this.props.title,
-      url: undefined
-    };
+  const disqusConfig = {
+    identifier: props.id ?? props.urlSlug,
+    title: props.title,
+    url: undefined
+  };
 
-    return (
-      <Layout
-        title={ `${this.props.title} - Blog` }
-        description={ this.props.description }
-        keywords={ this.props.tags.map(x => x.name).join(', ') }
-        canonicalUrl={ this.props.canonicalUrl }
-        pageId="blog">
+  return (
+    <Layout
+      title={ `${props.title} - Blog` }
+      description={ props.description }
+      keywords={ props.tags.map(x => x.name).join(', ') }
+      canonicalUrl={ props.canonicalUrl }
+      pageId="blog">
 
-        <Head>
-          <meta property="og:locale" content="en_US" />
-          <meta property="og:type" content="article" />
-          <meta property="og:url" content="@ViewContext.HttpContext.Request.GetDisplayUrl()" />
-          <meta property="og:title" content={this.props.title} />
-          {this.props.description && (<meta property="og:description" content={this.props.description} />)}
-          {this.props.image && (<meta property="og:image" content={imageUrl} />)}
-          <meta name="twitter:card" content={ this.props.image ? 'summary_large_image' : 'summary' } />
-          <meta name="twitter:site" content={appConfig.social.twitterLogin} />
-          <meta name="twitter:creator" content={appConfig.social.twitterLogin} />
-          <meta name="twitter:title" content={this.props.title} />
-          {this.props.description && (<meta name="twitter:description" content={this.props.description} />)}
-          {this.props.image && (<meta name="twitter:image" content={imageUrl} />)}
-          {!this.props.published && (<meta name="robots" content="noindex" />)}
+      <Head>
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="@ViewContext.HttpContext.Request.GetDisplayUrl()" />
+        <meta property="og:title" content={props.title} />
+        {props.description && (<meta property="og:description" content={props.description} />)}
+        {props.image && (<meta property="og:image" content={imageUrl} />)}
+        <meta name="twitter:card" content={ props.image ? 'summary_large_image' : 'summary' } />
+        <meta name="twitter:site" content={appConfig.social.twitterLogin} />
+        <meta name="twitter:creator" content={appConfig.social.twitterLogin} />
+        <meta name="twitter:title" content={props.title} />
+        {props.description && (<meta name="twitter:description" content={props.description} />)}
+        {props.image && (<meta name="twitter:image" content={imageUrl} />)}
+        {!props.published && (<meta name="robots" content="noindex" />)}
 
-          {/* Go to www.addthis.com/dashboard to customize your tools */}
-          <script type="text/javascript" src={`//s7.addthis.com/js/300/addthis_widget.js#pubid=${appConfig.socialIntegration.addThisPubId}`} async />
-        </Head>
+        {/* Go to www.addthis.com/dashboard to customize your tools */}
+        <script type="text/javascript" src={`//s7.addthis.com/js/300/addthis_widget.js#pubid=${appConfig.socialIntegration.addThisPubId}`} async />
+      </Head>
 
-        <div className="jumbotron page-header">
-          <div className="container">
-            <h1 itemProp="name">{this.props.title}</h1>
-            <p>
-              <i className={ this.props.published ? " fa fa-clock-o" : "glyphicon glyphicon-hourglass"}></i>&nbsp;
-              <span itemProp="datePublished">{ Converter.formatDate(new Date(this.props.postedOn)) }</span>
-            </p>
-          </div>
+      <div className="jumbotron page-header">
+        <div className="container">
+          <h1 itemProp="name">{props.title}</h1>
+          <p>
+            <i className={ props.published ? " fa fa-clock-o" : "glyphicon glyphicon-hourglass"}></i>&nbsp;
+            <span itemProp="datePublished">{ Converter.formatDate(new Date(props.postedOn)) }</span>
+          </p>
         </div>
+      </div>
 
-        <div className="container" itemScope itemType="http://schema.org/BlogPosting">
-          <section id="content" itemProp="blogPost" className="blog-post-content">
-            <BlogPostContent urlSlug={this.props.urlSlug} markdownContent={this.props.content} />
-          </section>
+      <div className="container" itemScope itemType="http://schema.org/BlogPosting">
+        <section id="content" itemProp="blogPost" className="blog-post-content">
+          <BlogPostContent urlSlug={props.urlSlug} markdownContent={props.content} />
+        </section>
 
-          <hr className="blog-post-footer-delimiter" />
+        <hr className="blog-post-footer-delimiter" />
 
-          <BlogPostFooter post={this.props} />
+        <BlogPostFooter post={props} />
 
-          {/* Go to www.addthis.com/dashboard to customize your tools */}
-          <div className="addthis_sharing_toolbox"></div>
+        {/* Go to www.addthis.com/dashboard to customize your tools */}
+        <div className="addthis_sharing_toolbox"></div>
 
-          <Disqus.DiscussionEmbed
-            shortname={appConfig.socialIntegration.disqusShortname}
-            config={disqusConfig} />
+        <Disqus.DiscussionEmbed
+          shortname={appConfig.socialIntegration.disqusShortname}
+          config={disqusConfig} />
 
-          <div className="scroll-to-top-button">
-            <i className="fa fa-arrow-circle-up" onClick={this.scrollToTop}></i>
-          </div>
+        <div className="scroll-to-top-button">
+          <i className="fa fa-arrow-circle-up" onClick={scrollToTop}></i>
         </div>
-      </Layout>
-    );
-  }
+      </div>
+    </Layout>
+  );
+}
 
-  private scrollToTop() {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-    return false;
-  }
+function scrollToTop() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
 }
