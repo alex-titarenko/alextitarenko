3f5c91971f8dca40a75830eba8bf6b570167a2f3
Alex Titarenko <alx.titarenko@gmail.com>
2025-01-11 19:39:45 -0800

    code refactoring

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,7 +6,51 @@ import { createUseStyles } from 'react-jss'
 
 const useStyles = createUseStyles({
   navBarContainer: {
-    display: 'flex'
+    display: 'flex',
+    alignItems: 'center'
+  },
+
+  navbarBrand: {
+    fontSize: '18px',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+    color: 'silver',
+    textShadow: '0 1px 2px #fff',
+    transition: 'color .3s ease-in',
+
+    '&:hover': {
+      textDecoration: 'none'
+    },
+
+    '& span': {
+      color: 'gray',
+      transition: 'color .3s ease-in'
+    }
+  },
+
+  toggleButton: {
+    position: 'relative',
+    padding: '9px 10px',
+    marginRight: '15px',
+    marginTop: '8px',
+    marginBottom: '8px',
+    backgroundColor: 'transparent',
+    backgroundImage: 'none',
+    border: '1px solid transparent',
+    borderRadius: '4px',
+    borderColor: '#ddd',
+  },
+
+  iconBar: {
+    display: 'block',
+    width: '22px',
+    height: '2px',
+    borderRadius: '1px',
+    backgroundColor: '#888',
+
+    '&+&': {
+      marginTop: '4px'
+    }
   }
 })
 
@@ -16,35 +60,30 @@ export function NavBar(props: { ref: React.RefObject<HTMLUListElement | null> })
   return (
     <header className="navbar navbar-default navbar-static-top">
       <div className={ clsx(classes.navBarContainer, "container") }>
-        <div className="navbar-header">
-          <button
-            type="button"
-            className="navbar-toggle collapsed"
-            data-toggle="collapse"
-            data-target="#navbar"
-            aria-expanded="false"
-            aria-controls="navbar"
-          >
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
-
-          <Link href="/" className="navbar-brand">
-            &lt; <span>{ appConfig.brandName }</span> /&gt;
-          </Link>
-        </div>
+        <Link href="/" className={ classes.navbarBrand }>
+          &lt; <span>{ appConfig.brandName }</span> /&gt;
+        </Link>
 
         <Spacer />
 
+        <button
+          type="button"
+          className={ classes.toggleButton }
+          aria-expanded="false"
+          aria-controls="navbar"
+        >
+          <span className={ classes.iconBar }></span>
+          <span className={ classes.iconBar }></span>
+          <span className={ classes.iconBar }></span>
+        </button>
+
         <nav id="navbar" className="collapse navbar-collapse bs-navbar-collapse">
           <ul id="mainmenu" ref={ props.ref } className="nav navbar-nav">
-            <li >
+            <li>
               <Link href="https://noteshub.app">NotesHub</Link>
             </li>
 
-            <li >
+            <li>
               <Link href="https://multicalculator.app">MultiCalc</Link>
             </li>
 
